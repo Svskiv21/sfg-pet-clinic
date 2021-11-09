fde5372f3640b29654edfd075adba8dd3ddf3239
Svskiv21 <nataliakrukar@gmail.com>
2021-11-09 17:04:06 +0100

    added VetController and page. Close #2

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -9,6 +9,6 @@ public class IndexController {
     @GetMapping(value = {"", "/","index", "index.html"})
     // when request comes to root context of root/index or index.html it all gonna match the request mapping
     public String index(){
-        return "index"; //  it is going to look back and go to index.html
-    }
+        return "index";
+    } //  it is going to look back and go to index.html
 }
