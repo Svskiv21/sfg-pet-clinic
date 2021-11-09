0440dd7ac45d9a12588cea04f84ede83a68c74b7
Svskiv21 <nataliakrukar@gmail.com>
2021-11-09 15:07:53 +0100

    added IndexController

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = {"", "/","index", "index.html"})
+    // when request comes to root context of root/index or index.html it all gonna match the request mapping
+    public String index(){
+        return "index"; //  it is going to look back and go to index.html
+    }
+}
